bfe0e2be7e95bf24d8ad7adf0043b22090923fe0
mohitdhingra05 <dhingramohit891@gmail.com>
2024-03-14 22:20:22 +0530

    Add Set and Map Files

---
+++ language/map.java
@@ -0,0 +1,22 @@
+package language;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+//...when ever you need to count the frequency of any integer or string
+public class map {
+    public static void main(String args[]){
+       
+
+        Scanner sc=new Scanner(System.in);
+        String str = sc.next();;
+
+        Map<Character,Integer> m= new HashMap<>();
+        
+        for(int i=0;i<str.length();i++){
+            
+        }
+
+    }
+}
---
+++ language/sets.java
@@ -0,0 +1,23 @@
+package language;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+// Sets are used to remove or mention uniqueness in the string
+public class sets{
+public static void main(String [] args){
+
+    Scanner sc= new Scanner(System.in);
+
+    String str= sc.next();
+
+    Set<Character>ch =new HashSet<>();
+
+    // for(char ch: "str")
+
+    int Length=str.length();
+
+   
+
+}
+}
